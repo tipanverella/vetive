236954ee11ac7f1c9135484b318c42656a024e6e
J. Tipan Verella <tipan.verella@gmail.com>
2022-08-03 11:29:56 -0400

    merge_sorted_lists

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,6 @@
 """ tests for init"""
 
-from vetive import get_env_var
+from vetive import get_env_var, merge_sorted_lists
 
 def test_get_env_var():
     """test of get_env_var"""
@@ -10,3 +10,14 @@ def test_get_env_var():
     assert ev_home is not None
     assert ev_user is not None
     assert ev_hsu is None
+
+
+def test_merge_sorted_lists():
+    """test of merge_sorted_lists"""
+    list1 = [1, 4, 5, 5, 6]
+    list2 = [2, 7, 11, 12, 12, 13]
+    expected_output = [1, 2, 4, 5, 5, 6, 7, 11, 12, 12, 13]
+    output = merge_sorted_lists(list1,list2)
+    assert output == expected_output
+    output = merge_sorted_lists(list2,list1)
+    assert output == expected_output
--- vetive/__init__.py
+++ vetive/__init__.py
@@ -3,11 +3,32 @@
     - get_env_var
 """
 
+from copy import deepcopy
 import os
-from typing import Optional
+from typing import List, Optional
 
 
 def get_env_var(env_var_name: str) -> Optional[str]:
     """gets an environment variable"""
     env_var = os.environ.get(env_var_name)
     return env_var
+
+
+def merge_sorted_lists(list1: List, list2: List) -> List:
+    """returns a sorted merge of the provided list"""
+    ell_one = deepcopy(list1)
+    ell_two = deepcopy(list2)
+
+    def _helper_generator():
+        while ell_one or ell_two:
+            if ell_one and ell_two:
+                if ell_one[0] < ell_two[0]:
+                    yield ell_one.pop(0)
+                else:
+                    yield ell_two.pop(0)
+            elif ell_one:
+                yield ell_one.pop(0)
+            else:
+                yield ell_two.pop(0)
+
+    return list(_helper_generator())
